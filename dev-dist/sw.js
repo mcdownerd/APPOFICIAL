f84276f31c3aca969953003cea12ed076a11ca96
Seu Nome Completo <nerd@admin.com>
2025-10-03 00:43:08 +0100

    [dyad] Atualizando RLS para permitir remoção de tickets por restaurante - wrote 2 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8b0af05'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b8641ctelo8"
+    "revision": "0.boarofkqfoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
