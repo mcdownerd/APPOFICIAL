af002c82eb7b55d41dffac5bb059059586889188
Seu Nome Completo <nerd@admin.com>
2025-10-31 11:44:19 +0000

    Reverted all changes back to version 067e28abc45e866400bb5af2d92a66eb8ce3f1d9

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8b0af05'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kbnj1f0n4g8"
+    "revision": "0.p44nhaar1gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
