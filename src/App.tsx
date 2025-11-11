982985faf88fe8ec52fd3e49ba8d04f9869261f0
Seu Nome Completo <nerd@admin.com>
2025-11-11 14:25:35 +0000

    [dyad] Fixed URL construction by adding basename to BrowserRouter - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <SessionContextProvider>
             <Routes>
               <Route path="/login" element={<Login />} />
