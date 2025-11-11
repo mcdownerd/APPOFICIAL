39894e2eb5bb485348073be3fd42ae36448f5d4e
Seu Nome Completo <nerd@admin.com>
2025-11-11 11:10:32 +0000

    [dyad] Corrigindo permissões de soft-delete para usuários 'restaurante' e 'estafeta' e adicionando tradução. - wrote 1 file(s), executed 2 SQL queries + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8b0af05'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1m11m8ta8kg"
+    "revision": "0.ld2g3biqn18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
